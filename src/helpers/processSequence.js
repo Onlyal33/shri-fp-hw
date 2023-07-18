b7ebf4e0fbe7f91c4564c005f75377c21774d14d
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-07-18 20:31:05 +0200

    part 2

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,113 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
 
- const api = new Api();
+/* Берем строку N. Пишем изначальную строку в writeLog.
+Строка валидируется по следующим правилам:
+кол-во символов в числе должно быть меньше 10.
+кол-во символов в числе должно быть больше 2.
+число должно быть положительным
+символы в строке только [0-9] и точка т.е. число в 10-ной системе счисления (возможно с плавающей запятой)
+В случае ошибки валидации вызвать handleError с 'ValidationError' строкой в качестве аргумента
+Привести строку к числу, округлить к ближайшему целому с точностью до единицы, записать в writeLog.
+C помощью API /numbers/base перевести из 10-й системы счисления в двоичную, результат записать в writeLog
+Взять кол-во символов в полученном от API числе записать в writeLog
+Возвести в квадрат с помощью Javascript записать в writeLog
+Взять остаток от деления на 3, записать в writeLog
+C помощью API /animals.tech/id/name получить случайное животное используя полученный остаток в качестве id
+Завершить цепочку вызовом handleSuccess в который в качестве аргумента положить результат полученный на предыдущем шаге
+ */
+import {
+  __,
+  allPass,
+  andThen,
+  assoc,
+  compose,
+  concat,
+  gt,
+  ifElse,
+  length,
+  lt,
+  mathMod,
+  otherwise,
+  partial,
+  prop,
+  tap,
+  test,
+} from 'ramda';
+import Api from '../tools/api';
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const api = new Api();
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const gtTwo = gt(__, 2);
+const ltTen = lt(__, 10);
+const lengthGreaterThanTwo = compose(gtTwo, length);
+const lengthLowerThanTen = compose(ltTen, length);
+const containsOnlyNumbers = test(/^[0-9]+\.?[0-9]+$/);
+const validate = allPass([
+  lengthGreaterThanTwo,
+  lengthLowerThanTen,
+  containsOnlyNumbers,
+]);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const stringToRoundedNumber = compose(Math.round, Number);
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const assocNumberParamToParams = assoc('number', __, { from: 10, to: 2 });
+const asyncApiGetBinaryNumber = compose(
+  api.get('https://api.tech/numbers/base'),
+  assocNumberParamToParams
+);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const getParamResult = compose(String, prop('result'));
+const asyncGetParamResult = andThen(getParamResult);
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const asyncGetLength = andThen(length);
+
+const square = (num) => num ** 2;
+const asyncSquare = andThen(square);
+
+const modThreeToString = compose(String, mathMod(__, 3));
+const asyncModThreeToString = andThen(modThreeToString);
+
+const asyncConcatIdToAnimalsUrl = andThen(concat('https://animals.tech/'));
+
+const asyncApiGetRequestWithEmptyParams = andThen(api.get(__, {}));
+
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  const tapLog = tap(writeLog);
+  const asyncTapLog = andThen(tapLog);
+  const asyncHandleSuccess = andThen(handleSuccess);
+  const asyncHandleError = otherwise(handleError);
+
+  const handleValidationError = partial(handleError, ['ValidationError']);
+
+  const mainSequence = compose(
+    asyncHandleError,
+    asyncHandleSuccess,
+    asyncGetParamResult,
+    asyncApiGetRequestWithEmptyParams,
+    asyncConcatIdToAnimalsUrl,
+    asyncTapLog,
+    asyncModThreeToString,
+    asyncTapLog,
+    asyncSquare,
+    asyncTapLog,
+    asyncGetLength,
+    asyncTapLog,
+    asyncGetParamResult,
+    asyncApiGetBinaryNumber,
+    tapLog,
+    stringToRoundedNumber
+  );
+
+  const runValidated = ifElse(
+    validate,
+    mainSequence,
+    handleValidationError
+  );
+  const run = compose(runValidated, tapLog);
+
+  run(value);
+};
 
 export default processSequence;
